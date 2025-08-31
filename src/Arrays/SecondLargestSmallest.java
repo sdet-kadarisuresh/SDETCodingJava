a8d1df92f87a497a0f8ea61d3801e26d0364fb0b
sdet-kadarisuresh <kadarisureshsdet@gmail.com>
2025-08-31 21:42:03 +0530

    first commit

---
+++ src/Arrays/SecondLargestSmallest.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SecondLargestSmallest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr= {2,3,4,33,42,5,44,36,75,57};
+		
+		//Arrays.sort(arr);
+		
+		//System.out.println(arr[arr.length-2]);
+		//System.out.println(arr[1]);
+		
+	}
+
+}
