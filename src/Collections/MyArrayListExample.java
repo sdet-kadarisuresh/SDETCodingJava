8b50fe6c24936404a855e49a9a7ed05afa5ec523
sdet-kadarisuresh <kadarisureshsdet@gmail.com>
2025-09-04 06:28:45 +0530

    new changes

---
+++ src/Collections/MyArrayListExample.java
@@ -0,0 +1,22 @@
+package Collections;
+
+
+import java.util.*;
+public class MyArrayListExample  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        
+		ArrayList<String> ls=new ArrayList<>();
+		ls.add("a");
+		ls.add("b");
+		ls.add("c");
+		
+		
+		
+		
+		
+	}
+
+}
